e795518d328c82379189dd6ba8e75ad43810c896
Catherine Rodriguez <catherinebrodriguez@gmail.com>
2017-12-02 22:55:45 -0500

    Add files via upload

--- CustomRenderer.java
+++ CustomRenderer.java
@@ -14,20 +14,21 @@ public class CustomRenderer extends DefaultTableCellRenderer
 	private static final long serialVersionUID = 1L;
 	private Space[][] map;  // the list of rooms/walls in the maze
 	private RandomMazeClass maze; // the mazeclass
-	//private int num;
+	private int num;
 	
 	public CustomRenderer()
 	{
 		maze = null;
+		 
 		map = null;
-		//num = 0;
+		
 	}
 	
-	public CustomRenderer(RandomMazeClass maze, Space[][] map)
+	public CustomRenderer(RandomMazeClass maze, Space[][] map, boolean gameEnd)
 	{
 		this.maze = maze;
 		this.map = map;
-		//num = 255;
+		num = 255;
 	}
 	
 	/*
@@ -42,19 +43,24 @@ public class CustomRenderer extends DefaultTableCellRenderer
 		Component rendererComp = super.getTableCellRendererComponent(table, value, 
 				isSelected, hasFocus, row, col);
 
+		
 		// Set alignment and design of cells
 		rendererComp.setForeground(Color.white);
 		setHorizontalAlignment(JLabel.CENTER); 
 		setVerticalAlignment(JLabel.CENTER); 
 		 
+		
 		table.setCellSelectionEnabled(true);
+		//System.out.println(num);
+		 
 		 
 		// if cell is a border wall, disable selection and paint wall color
 		if ((col == 0 || col == table.getColumnCount() - 1) 
 				|| (row == 0 || row == table.getRowCount() - 1))
 		{
-			table.setCellSelectionEnabled(false);
-    		rendererComp.setBackground(new Color(204, 204, 255));
+			//table.setCellSelectionEnabled(false);
+			 
+				rendererComp.setBackground(new Color(255-row, 204, 255));
 		}
 		// else if the start or end cell in the maze bounds
 		else if((row >= 1 && col >= 1) 
@@ -69,13 +75,15 @@ public class CustomRenderer extends DefaultTableCellRenderer
         	else if(map[col-1][row-1].getRoomNum() == maze.getNumRooms()-1)
         	{
         		rendererComp.setBackground(Color.magenta);
+        		//maze.setGameEnd(true);
         		setText("E");
         	}
 			// else if an interior wall cell, disable selection
         	else if(map[col-1][row-1].getRoomNum() == -1)
 			{
-        		table.setCellSelectionEnabled(false);
-        		rendererComp.setBackground(new Color(204, 204, 255));
+
+    				rendererComp.setBackground(new Color(255-row, 204, 255));
+        	 
 			}
 			// else an empty room cell
 			else
@@ -83,17 +91,21 @@ public class CustomRenderer extends DefaultTableCellRenderer
 				rendererComp.setForeground(Color.magenta);
 				rendererComp.setBackground(new Color(245, 245, 245));
 			}
-			// if the current cell is selected, paint cell pink
+			// if the current cell is selected by player, paint cell blue
 			if(table.isCellSelected(row, col))
         	{ 
-				rendererComp.setBackground(Color.pink);
+				 if((map[col-1][row-1].getRoomNum() == maze.getNumRooms()-1))
+				 {	 
+					// System.out.println("END!");	
+					 maze.setGameEnd(true);
+					 
+				 }
+				// color current selected cell blue where player is
+				rendererComp.setBackground(new Color(10, 204, 255));
         	}
+			 
 		} // end if in maze bounds
-		
-		//if(num > 1)
-		//	num = num-2;
-		//else
-		//	num = 255;
+		 
 		 
 		return rendererComp;
 
--- MazeGame.java
+++ MazeGame.java
@@ -1,17 +1,31 @@
 package mazeProject;
- 
+
 import java.awt.Color;
-import java.awt.Font; 
+import java.awt.Component;
+import java.awt.Font;
 import java.awt.Dimension;
-import java.awt.EventQueue; 
-import javax.swing.*; 
-import javax.swing.border.*;
+import java.awt.EventQueue;
+import javax.swing.*;
+import javax.swing.border.*; 
+
 import net.miginfocom.swing.MigLayout;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent; 
+import java.awt.Insets;
+ 
+
 public class MazeGame
-{ 
+{
 	private JFrame frame;   // outer frame of gui
 	private JTable table;   // table to store maze
+	private JPanel panel;
+	private JPanel mazeMap;
+	private JLabel lblTop;
+	private RandomMazeClass newMaze;
+	private Space[][] map;
 	 
 	/**
 	 * Launch the application.
@@ -40,20 +54,108 @@ public class MazeGame
 	 * Create the application.
 	 */
 	public MazeGame()
-	{
+	{ 
+		generateMaze();
+		generateTable();
 		initialize();
 	}
+
+	
+
+	public void generateMaze()
+	{
+		newMaze = new RandomMazeClass(21, 21);
+		map = newMaze.getMap(); // list of rooms
+
+	} // end generateMaze
+
+	public void generateTable()
+	{
+		// table = new JTable(23, 23);
+		table = new JTable(new MyTableModel(23, 23))
+		// create table to display maze
+		// (should be mazeSize+2 to include borders, not static 23)
+		// table = new JTable(23, 23)
+		{
+
+			// setColCount(23);
+			// add default serialID to remove warning
+			private static final long serialVersionUID = 1L;
+
+			// override the changeSelection method to allow user to only
+			// select valid rooms within the maze bounds that are 1 away from
+			// the currently selected cell
+			@Override
+			public void changeSelection(int row, int col, boolean toggle,
+					boolean expand)
+			{
+				
+				// get the currently selected cell position values
+				int currRow = table.getSelectedRow();
+				int currCol = table.getSelectedColumn();
+				if ((row >= 1 && col >= 1) 
+						&& (row < table.getRowCount() - 1
+						&& col < table.getColumnCount() - 1) 
+						&& map[col - 1][row - 1].getRoomNum() == newMaze.getNumRooms()-1)
+					{ 
+						winGame();
+						 
+					}
+				// if new space selection is a valid, contiguous space in maze
+				// then update to the new selected cell position
+				if ((row >= 1 && col >= 1) && (row < table.getRowCount() - 1
+						&& col < table.getColumnCount() - 1))
+					if ((map[col - 1][row - 1].getRoomNum() != -1)
+							&& ((Math.abs(currRow - row) == 1
+									|| Math.abs(currRow - row) == 0)
+									&& (Math.abs(currCol - col) == 1
+											|| Math.abs(currCol - col) == 0)))
+						super.changeSelection(row, col, toggle, expand);
+
+				 
+				
+			}  // end changeSelection
+
+		}; // end table cell selection configuration
+		table.setBackground(new Color(255, 255, 255));
+		// table.getSelectionModel().addListSelectionListener(yourListener);
+		// set table selection to single cell selection
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+
+		// start at cell 1,1 in table (start position)
+		table.setRowSelectionInterval(1, 1);
+		table.setColumnSelectionInterval(1, 1);
+		table.setUpdateSelectionOnSort(false);
+		table.setRowSelectionAllowed(false);
+		table.setIgnoreRepaint(false);
+
+		// set table cell design, height, margins
+		table.setForeground(Color.WHITE);
+		table.setRowHeight(30);
+		table.setIntercellSpacing(new Dimension(0, 0));
+		
+		// set the column width size for each column in table
+		for (int i = 0; i < 23; i++)
+		{
+			table.getColumnModel().getColumn(i).setPreferredWidth(30);
+			table.getColumnModel().getColumn(i)
+					.setCellRenderer(new CustomRenderer(newMaze, map, newMaze.isGameEnd()));
+
+		}
+
+	} // end generateTable
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize()
 	{
-		// create new maze on game load 
-		//(using size 21 now but should allow user to choose difficulty)
-		RandomMazeClass newMaze = new RandomMazeClass(21, 21);
-		Space[][] map = newMaze.getMap(); // list of rooms
- 
+		Font btnFont = new Font("Segoe UI Historic", Font.PLAIN, 28);
+
+		// create new maze on game load
+		// (using size 21 now but should allow user to choose difficulty)
+		// generateMaze();
+
 		// main application frame
 		frame = new JFrame();
 		frame.setResizable(false);
@@ -64,92 +166,79 @@ public class MazeGame
 				new MigLayout("wrap 1", "[grow,fill]", "[grow,fill]"));
 
 		// main panel to display game contents
-		JPanel panel = new JPanel();
+		panel = new JPanel();
 		panel.setBorder(new EmptyBorder(10, 10, 10, 10));
 		panel.setBackground(new Color(90, 80, 150));
-		panel.setLayout(new MigLayout("wrap 1", "[grow,fill]",
-				"[grow,fill][grow,fill]"));
+		panel.setLayout(new MigLayout("wrap 1", "[grow][grow,fill]",
+				"[grow][grow,fill][grow,fill]"));
 
 		// panel to display maze
-		JPanel mazeMap = new JPanel();
+		mazeMap = new JPanel();
 		mazeMap.setBackground(new Color(90, 80, 150));
 		mazeMap.setLayout(new MigLayout("", "[grow,fill]", "[grow,fill]"));
-
-		// create table to display maze 
-		//(should be mazeSize+2 to include borders, not static 23)
-		table = new JTable(23, 23)
-		{
-			/**
-			 * 
-			 */
-			// add default serialID to remove warning
-			private static final long serialVersionUID = 1L;
-
-			// override the changeSelection method to allow user to only
-			// select valid rooms within the maze bounds that are 1 away from
-			// the currently selected cell
-			@Override
-			public void changeSelection(int row, int col, boolean toggle, boolean expand)
-			{
-				// get the currently selected cell position values
-				int currRow = table.getSelectedRow();
-				int currCol = table.getSelectedColumn();
-
-				// if new space selection is a valid, contiguous space in maze
-				// then update to the new selected cell position
-				if((row >= 1 && col >= 1) && (row < table.getRowCount() - 1 && col < table.getColumnCount() - 1))
-					if ((map[col - 1][row - 1].getRoomNum() != -1)
-						&& ((Math.abs(currRow - row) == 1 || Math.abs(currRow - row) == 0)
-						&&  (Math.abs(currCol - col) == 1 || Math.abs(currCol - col) == 0)))
-							super.changeSelection(row, col, toggle, expand);
-
-			}  // end changeSelection
-
-			// make all cells non-editable
-			public boolean isCellEditable(int row, int column)
-			{
-				return false;
-			}
-
-		}; // end table cell selection configuration
-
-		// set table selection to single cell selection
-		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-		
-		// start at cell 1,1 in table (start position)
-		table.setRowSelectionInterval(1, 1);
-		table.setColumnSelectionInterval(1, 1);
-		table.setUpdateSelectionOnSort(false);
-		table.setRowSelectionAllowed(false);
-		table.setIgnoreRepaint(true);
-
-		// set table cell design, height, margins 
-		table.setForeground(Color.WHITE);
-		table.setRowHeight(30);						    
-		table.setIntercellSpacing(new Dimension(0, 0)); 
-	 
-		// set the column width size for each column in table
-		for (int i = 0; i < 23; i++)
-		{
-			table.getColumnModel().getColumn(i).setPreferredWidth(30);
-			table.getColumnModel().getColumn(i)
-					.setCellRenderer(new CustomRenderer(newMaze, map));
-		}
 
 		// add maze map table to maze panel
 		mazeMap.add(table, "cell 0 0,alignx center,aligny center");
 
+		JButton btnRestart = new JButton("New Maze");
+		btnRestart.setVisible(false);
+		btnRestart.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{
+				mazeMap.remove(table);
+				 
+				generateMaze();
+				generateTable();
+ 
+				mazeMap.add(table);
+				table.setIntercellSpacing(new Dimension(0, 0));
+ 
+				btnRestart.transferFocusBackward();
+			}
+
+		}); // end btnStart mouseListener
+		
+		JButton btnStart = new JButton("Start!");
+		
+		btnStart.setFont(new Font("Segoe UI Historic", Font.PLAIN, 28));
+		btnStart.setMargin(new Insets(20, 20, 20, 20));
+		btnStart.addMouseListener(new MouseAdapter()
+		{
+			@Override
+			public void mouseClicked(MouseEvent arg0)
+			{
+				
+				btnStart.setVisible(false);
+				btnRestart.setVisible(true);
+				table.setVisible(true);
+				panel.add(mazeMap, "cell 0 1,alignx center");
+				panel.add(btnRestart, "cell 0 2,alignx center");
+				for (Component comp : panel.getComponents())
+				{
+					// remove text focus border and add hover effect
+					if (comp instanceof JButton)
+					{
+						((JButton) comp).setFocusPainted(false);
+						createNewButton((JButton) comp);
+					}
+				} // end for each button in buttons
+				panel.remove(btnStart);
+				btnRestart.transferFocusBackward();
+				//System.out.print(table.getValueAt(1, 1));
+				frame.pack();
+				frame.setLocationRelativeTo(null);
+			}
+
+		}); // end btnStart mouseListener
+		 
 		// player info will go here add ....
 		JLabel lblPlayerName = new JLabel("PLAYER!");
 
 		lblPlayerName.setFont(new Font("Calibri", Font.BOLD, 28));
 		JLabel lblScore = new JLabel("Score is 20");
-
-		// button to restart game
-		JButton btnRestart = new JButton("Restart");
-		btnRestart.setBorder(new LineBorder(Color.LIGHT_GRAY, 1, true));
-		btnRestart.setFont(new Font("Calibri", Font.PLAIN, 24));
-
+ 
 		// information panel to display player info and buttons
 		JPanel displayText = new JPanel();
 		displayText.setLayout(
@@ -158,13 +247,27 @@ public class MazeGame
 		// add info buttons/labels to display panel
 		displayText.add(lblPlayerName);
 		displayText.add(lblScore);
-		displayText.add(btnRestart);
 
+		lblTop = new JLabel("ESCAPE THE MAZE!");
+		lblTop.setFont(new Font("Segoe UI Historic", Font.BOLD, 28));
+		lblTop.setForeground(Color.white);
+		panel.add(lblTop, "cell 0 0,alignx center");
+		 
+		// table initially not visible to screen
+		table.setVisible(false);
 		// add maze and info panels to main panel
-		panel.add(mazeMap, "alignx center");
-		panel.add(displayText,
-				"cell 0 1,growx,sizegroupx ,sizegroupy ,aligny top");
-
+		panel.add(btnStart, "cell 0 1,alignx center");
+		//btnStart.setMargin(new Insets(20, 20, 20, 20));
+		for (Component comp : panel.getComponents())
+		{
+			// remove text focus border and add hover effect
+			if (comp instanceof JButton)
+			{
+				((JButton) comp).setFocusPainted(false);
+				createNewButton((JButton) comp);
+			}
+		} // end for each button in buttons
+		 
 		// add panel to frame and size to screen
 		frame.getContentPane().add(panel, "cell 0 0,growx,aligny top");
 		frame.pack();
@@ -172,4 +275,41 @@ public class MazeGame
 
 	} // end initialize
 
+	public void createNewButton(JButton btn)
+	{
+		Font btnFont = new Font("Segoe UI Historic", Font.BOLD, 28);
+ 
+		btn.setForeground(new Color(255, 255, 255));
+		btn.setBackground(new Color(246, 61, 162));
+		btn.setFont(btnFont);
+		btn.setMargin(new Insets(10,20,10,20));
+		btn.setBorderPainted(false);
+		String btnText = btn.getText();
+
+		btn.addMouseListener(new MouseAdapter()
+		{
+			public void mouseEntered(java.awt.event.MouseEvent e)
+			{
+				JButton button = (JButton) e.getComponent();
+				button.setBackground(new Color(230, 10, 130));
+				
+			}
+
+			public void mouseExited(java.awt.event.MouseEvent e)
+			{
+				JButton button = (JButton) e.getComponent();
+				button.setBackground(new Color(246, 61, 162));
+			}
+		});
+ 
+	} // end createNewButton
+
+	public void winGame()
+	{
+		lblTop.setText("YOU WON!");
+		
+		table.setFocusable(false);
+		table.setRowSelectionAllowed(false);
+	}
+	
 } // end MazeGame
